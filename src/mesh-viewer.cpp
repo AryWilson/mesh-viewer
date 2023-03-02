f2b97b6dbddf84c12b69308f744b541805279e4a
AryWilson <awilson2@brynmawr.edu>
2023-03-02 16:04:54 -0500

    untested

--- src/mesh-viewer.cpp
+++ src/mesh-viewer.cpp
@@ -18,13 +18,22 @@ using namespace agl;
 class MeshViewer : public Window {
 public:
    MeshViewer() : Window() {
+      models = GetFilenamesInDir("../models", "ply");
    }
 
    void setup() {
       // mesh.load("../models/cube.ply");
+      // mesh.load(models[currentModel]);
+
    }
 
    void mouseMotion(int x, int y, int dx, int dy) {
+      if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
+         //update angle?
+         // x = Radius * sin(Azimuth) * cos(Elevation)
+         // y = Radius * sin(Elevation)
+         // z = Radius * cos(Azimuth) * cos(Elevation)
+      }
    }
 
    void mouseDown(int button, int mods) {
@@ -34,10 +43,26 @@ public:
    }
 
    void scroll(float dx, float dy) {
+      Radius+=dy;
    }
 
    void keyUp(int key, int mods) {
+
+      if (key == 78 || key == 110){
+         currentModel= (currentModel+1)% (models.size());
+
+      } else if (key == 80|| key == 112){
+         currentModel= (currentModel-1)% (models.size());
+      }
    }
+
+// lookAt() changes the camera position and orientation
+// translate() applies a translation transformation
+// rotate() applies a rotation transformation
+// scale() applies a scale transformation
+
+
+
 
    void draw() {
       float aspect = ((float)width()) / height();
@@ -46,6 +71,7 @@ public:
       renderer.rotate(vec3(0,0,0));
       renderer.scale(vec3(1,1,1));
       renderer.translate(vec3(0,0,0));
+      // mesh.load(models[currentModel]);
       //renderer.mesh(mesh);
       renderer.cube(); // for debugging!
    }
@@ -55,6 +81,11 @@ protected:
    vec3 eyePos = vec3(10, 0, 0);
    vec3 lookPos = vec3(0, 0, 0);
    vec3 up = vec3(0, 1, 0);
+   vector<string> models;
+   int currentModel;
+   float Radius;
+   float Azimuth;
+   float Elevation;
 };
 
 int main(int argc, char** argv)
--- src/plymesh.cpp
+++ src/plymesh.cpp
@@ -3,7 +3,10 @@
 // Date:
 // Description: Loads PLY files in ASCII format
 //--------------------------------------------------
-
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <regex>
 #include "plymesh.h"
 
 using namespace std;
@@ -31,24 +34,107 @@ namespace agl {
          std::cout << "WARNING: Cannot load different files with the same PLY mesh\n";
          return false;
       }
+      fstream file;
+      file.open(filename, ios::in);
+      if(!file.is_open()){
+         cerr << "could not open file" << endl;
+         file.close();
+         return false;
+      }
+      string line;
+      getline(file,line);
+      if(!line.compare("ply")){
+         cerr << "mesh file not ply" << endl;
+         file.close();
+         return false;
+      }
+      int nvertices;
+      int nfaces;
+      regex re_v("element vertex ([0-9]+)");
+      regex re_f("element face ([0-9]+)");
+      regex end_h("end_header");
+      regex re_vn("(-?[0-9]+\\.?[0-9]*) (-?[0-9]+\\.?[0-9]*) (-?[0-9]+\\.?[0-9]*) (-?[0-9]+\\.?[0-9]*) (-?[0-9]+\\.?[0-9]*) (-?[0-9]+\\.?[0-9]*)");
+      regex re_fn("3 (-?[0-9]+) (-?[0-9]+) (-?[0-9]+)");
+      smatch matches;
+      
+      while(getline(file,line)){
+         if(regex_search(line, matches, re_v)) {
+            nvertices = stoi(matches[0]);
+         }
+         if(regex_search(line, matches, re_f)) {
+            nfaces = stoi(matches[0]);
+
+         }
+         if(regex_search(line, end_h)) {
+            break;
+         }
+      }   
+      init();
+      //&_faces, &_positions, &_normals      
+      for(int i = 0; i<nvertices;){
+
+         if(!getline(file,line)){
+            cerr << "too few lines" << endl;
+            file.close();
+            return false;
+         }
+         
+         if(regex_search(line, matches, re_vn)) {
+            i++;
+            _positions.push_back(stof(matches[0]));
+            _positions.push_back(stof(matches[1]));
+            _positions.push_back(stof(matches[2]));
+            _normals.push_back(stof(matches[3]));
+            _normals.push_back(stof(matches[4]));
+            _normals.push_back(stof(matches[5]));
+         }
+      }
+      for(int i = 0; i<nfaces;){
+         if(!getline(file,line)){
+            cerr << "too few lines" << endl;
+            file.close();
+            return false;
+         }
+         if(regex_search(line, matches, re_fn)) {
+            i++;
+            _faces.push_back(stoi(matches[0]));
+            _faces.push_back(stoi(matches[1]));
+            _faces.push_back(stoi(matches[2]));
+         }
+
+      }
+      file.close();
+
       // todo: your code here
       return false;
    }
 
    glm::vec3 PLYMesh::minBounds() const {
-      return glm::vec3(0);
+      GLfloat x,y,z;
+      for(int i = 0; i<_positions.size(); i=i+3){
+         x = std::min(x,_positions[i+0]);
+         y = std::min(y,_positions[i+1]);
+         z = std::min(z,_positions[i+2]);
+      }
+      return glm::vec3(x,y,z);
    }
 
    glm::vec3 PLYMesh::maxBounds() const {
-      return glm::vec3(0);
+      GLfloat x,y,z;
+      for(int i = 0; i<_positions.size(); i=i+3){
+         x = std::max(x,_positions[i+0]);
+         y = std::max(y,_positions[i+1]);
+         z = std::max(z,_positions[i+2]);
+      }
+      return glm::vec3(x,y,z);
    }
 
    int PLYMesh::numVertices() const {
-      return _positions.size();
+      return _positions.size()/3;
    }
 
    int PLYMesh::numTriangles() const {
-      return _faces.size();
+      return _faces.size()/3;
    }
 
    const std::vector<GLfloat>& PLYMesh::positions() const {
